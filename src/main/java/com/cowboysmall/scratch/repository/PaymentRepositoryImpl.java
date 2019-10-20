3d4841304a160e1d9e4db10cd8e4dcd988c8e682
Jerry Kiely <jerry@cowboysmall.com>
2019-10-20 17:11:03 +0100

    Oops! Forgot to push this enhancement - added lock for concurrent access

--- src/main/java/com/cowboysmall/scratch/repository/PaymentRepositoryImpl.java
+++ src/main/java/com/cowboysmall/scratch/repository/PaymentRepositoryImpl.java
@@ -7,6 +7,8 @@ import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static java.lang.String.format;
 
@@ -14,6 +16,8 @@ public class PaymentRepositoryImpl implements PaymentRepository {
 
     private static final BigDecimal DAILY_LIMIT = new BigDecimal(50000);
     private static final SimpleDateFormat FORMAT = new SimpleDateFormat("ddMMyyyy");
+
+    private final Lock lock = new ReentrantLock();
 
     private ConcurrentMap<String, ConcurrentMap<String, ConcurrentMap<String, Payments>>> dataStore = new ConcurrentHashMap<>();
 
@@ -27,6 +31,27 @@ public class PaymentRepositoryImpl implements PaymentRepository {
         String currency = payment.getCurrency().getCurrencyCode();
         String creditor = payment.getCreditor();
 
+        lock.lock();
+        try {
+
+            Payments payments = getPayments(date, currency, creditor);
+
+            if (payments.getTotal().add(payment.getAmount()).compareTo(DAILY_LIMIT) > 0)
+                throw new PaymentRepositoryException(format("Daily Payments Total Exceeded For %s In %s.", creditor, currency));
+
+            payments.add(payment);
+
+        } finally {
+
+            lock.unlock();
+        }
+    }
+
+
+    //_________________________________________________________________________
+
+    private Payments getPayments(String date, String currency, String creditor) {
+
         if (!dataStore.containsKey(creditor))
             dataStore.put(creditor, new ConcurrentHashMap<>());
 
@@ -36,11 +61,6 @@ public class PaymentRepositoryImpl implements PaymentRepository {
         if (!dataStore.get(creditor).get(date).containsKey(currency))
             dataStore.get(creditor).get(date).put(currency, new Payments());
 
-        Payments payments = dataStore.get(creditor).get(date).get(currency);
-
-        if (payments.getTotal().add(payment.getAmount()).compareTo(DAILY_LIMIT) > 0)
-            throw new PaymentRepositoryException(format("Daily Payments Total Exceeded For %s In %s.", creditor, currency));
-
-        payments.add(payment);
+        return dataStore.get(creditor).get(date).get(currency);
     }
 }
