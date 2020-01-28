f86e1fbe1402f546491c93ed4fcdff2833260a2e
Jerry Kiely <jerry@cowboysmall.com>
2020-01-28 22:30:24 +0000

    cleanup of driver

--- src/main/java/com/cowboysmall/scratch/Driver.java
+++ src/main/java/com/cowboysmall/scratch/Driver.java
@@ -1,8 +1,7 @@
 package com.cowboysmall.scratch;
 
-import java.lang.reflect.Method;
-
 import static java.util.Arrays.copyOfRange;
+
 
 public class Driver {
 
@@ -11,13 +10,12 @@ public class Driver {
         if (args.length > 0) {
 
             Class<?> clazz = Class.forName(args[0]);
-            Object instance = clazz.getDeclaredConstructor().newInstance();
-            Method main = clazz.getMethod("main", String[].class);
 
-            if (args.length > 1)
-                main.invoke(instance, (Object) copyOfRange(args, 1, args.length + 1));
-            else
-                main.invoke(instance, (Object) new String[0]);
+            clazz.getMethod("main", String[].class)
+                    .invoke(
+                            clazz.getDeclaredConstructor().newInstance(),
+                            (Object) copyOfRange(args, 1, args.length)
+                    );
         }
     }
 }
