543c750c7bf89ad1c5c700ee7abab4e55e1e5077
Jerry Kiely <jerry@cowboysmall.com>
2023-06-08 20:12:11 +0100

    made some changes

--- src/main/java/com/cowboysmall/scratch/Application.java
+++ src/main/java/com/cowboysmall/scratch/Application.java
@@ -1,9 +1,28 @@
 package com.cowboysmall.scratch;
 
+import com.cowboysmall.scratch.util.EncDec;
+import com.cowboysmall.scratch.util.InterleavedIterator;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+
 public class Application {
 
     public static void main(String... args) {
 
         System.out.println(">>> Hello, World!");
+
+        System.out.println(">>> \"vmxibkgrlm\" - > " + EncDec.decode("vmxibkgrlm"));
+
+        Iterator<String> iterator =
+                new InterleavedIterator<>(
+                        Arrays.asList("a", "b", "c", "d").iterator(),
+                        Collections.emptyIterator(),
+                        Arrays.asList("foo", "bar").iterator()
+                );
+
+        while (iterator.hasNext())
+            System.out.println(iterator.next());
     }
 }
